9d812c6df81677c89cebd04e8f1b725f005fcbe1
Małgorzata Kierys <kierys.malgorzata@gmail.com>
2022-07-20 22:42:11 +0200

    New tutorial done

---
+++ Tutorial 7.py
@@ -0,0 +1,37 @@
+print(3 + 4.5)
+print(3 - 4.5)
+print(3 / 4.5)
+print(3 * 4.5)
+print(3 * 4.5 +6)
+print(3 * (4.5 +6))
+print(3 % 4.5)
+
+number = 7
+print(number)
+
+print(str(number) + " is my fav num.") #need to use str before variable number
+
+#using absolute value of number
+
+number = -7
+print(abs(number))
+
+#powering the numbers
+
+number = 7
+print(pow(number, 3))
+
+#rounding numbers
+
+number = 7.8
+print(round(number))
+
+
+#Python math
+
+from math import *
+
+number = 9
+print(floor(number))
+print(ceil(number))
+print(sqrt(number)) #pierwiastek
