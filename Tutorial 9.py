eb7e12d80174e70e70e286b6e345fb24a11d9311
Małgorzata Kierys <kierys.malgorzata@gmail.com>
2022-07-20 22:58:06 +0200

    Tuto 9

---
+++ Tutorial 9.py
@@ -0,0 +1,14 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2) #converting strings into numbers
+
+print("The result is: " + str(result)) #converting result from number into string
+
+#converting total numbers to decimal numbers
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2) #converting strings for numbers
+
+print("The result is: " + str(result)) #converting result from number to string
+
