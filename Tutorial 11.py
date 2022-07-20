4fe0681c585dbdbc6f0dfff974aa3f9f9f8cabc7
Małgorzata Kierys <kierys.malgorzata@gmail.com>
2022-07-20 23:30:32 +0200

    Tuto 11

---
+++ Tutorial 11.py
@@ -0,0 +1,8 @@
+
+shopping = ["Apples", "Bananas", "Plums", "Milk", "Ice cream"]
+ #              0         1          2      3           4
+shopping[3] = "Orange juice"
+print(shopping[2])
+print(shopping[-3]) #using negative values means that the programm is counting backwords
+
+print(shopping[1:4])
