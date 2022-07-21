d80a8b1c9a2b90c36ac98828574c96414e13cd4b
Małgorzata Kierys <kierys.malgorzata@gmail.com>
2022-07-21 18:51:26 +0200

    Tuto 14

---
+++ Tutorial 14.py
@@ -0,0 +1,23 @@
+
+def say_hi():
+    print("Hello You!")
+
+'''
+It's the function. It is using to safe order in our code. When we define it we have to call it 
+'''
+
+say_hi()
+
+#It is executing not there where it is define, but there where we call it
+
+print("Top")
+say_hi()
+print("Bottom")
+
+#parameters
+def say_hi(name):
+    print("Hello " + name + "!")
+
+say_hi("margo")
+
+#parametrers are useful to write it into function and thank of it is less code and better order
