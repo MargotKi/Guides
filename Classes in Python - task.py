0d71a57cb844b91a4fd252a9de9bd77c2a24ab5d
Małgorzata Kierys <kierys.malgorzata@gmail.com>
2022-08-04 22:37:48 +0200

    Return statement - task

---
+++ Classes in Python - task.py
@@ -0,0 +1,27 @@
+class Worker:
+
+    def _init_(self, name, last_name, age):
+        self.name = name
+        self.last_name = last_name
+        self.age = int(age)
+
+
+    def getWorker(str):
+
+        str.name = ["Katarzyna", "Lukasz", "Mateusz"]
+        str.last_name = ["Kowalska", "Lipa", "Wieczorek"]
+        str.age = [27, 38, 42]
+
+
+        #Worker[1] =
+    #@classmethod
+
+    #i = 0
+    #while i < len(self.name):
+    #    print(self.name[i], self.last_name[i], self.age)
+    #    i += 1
+
+def main():
+  
+   
+   
---
+++ Pracownicy firmy.py
@@ -0,0 +1,35 @@
+class Pracownik:
+
+    def __init__(self, imie, nazwisko, wiek, pracownik):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.wiek = wiek
+        self.pracownik = pracownik[3]
+
+    def nazwij(self):
+        self.imie = ["Katarzyna", "Lukasz", "Mateusz"]
+        self.nazwisko = ["x", "y"]
+        self.wiek = [2, 5]
+
+       # self.pracownik[0] = (self.imie[0], self.nazwisko[0], self.wiek[0])
+        #self.pracownik[1] = (self.imie[1], self.nazwisko[1], self.wiek[1])
+        #self.pracownik[2] = (self.imie[2], self.nazwisko[2], self.wiek[2])
+
+
+   # def nowy_pracownik(self):
+
+        print(self.imie[1])
+
+    poi = self.imie[1]
+    # pracownik[] =
+    print("gwhuiwec" {nazwij:(poi)})
+
+nazwij()
+
+def main():
+
+    #poi = self.imie[1]
+   # pracownik[] =
+    #print("gwhuiwec" {nazwij(poi)})
+
+main()
